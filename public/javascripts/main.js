fe36cc56870ed957e4ee9957ee0b32470d158ed0
suhotang <dkdudkc22@gmail.com>
2022-05-22 23:37:56 +0900

    Fix   - 모바일 메뉴 버튼 애니메이션 효과 추가   - animation 관련 스타일 파일 분리   - html 태그 시멘틱 태그로 변경 (일부분만)

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,14 +1,19 @@
 function onMenuClick() {
   const menuBtnElement = document.getElementById("menu-icon-btn")
   const fullMenuViewElement = document.getElementById("full-menu-view")
+  const menuIcon = document.getElementById("menuIcon")
+  const menuCloseIcon = document.getElementById("menuCloseIcon")
 
   const className = menuBtnElement.getAttribute("class") ?? ""
   if (className.includes("notOpen")) {
     fullMenuViewElement.setAttribute("class", "open-full-menu-view")
     menuBtnElement.setAttribute("class", "open")
+    menuIcon.setAttribute("class", "menuIconHide")
+    menuCloseIcon.setAttribute("class", "menuIconShow")
   } else if (className.includes("open")) {
     fullMenuViewElement.setAttribute("class", "close-full-menu-view")
     menuBtnElement.setAttribute("class", "notOpen")
+    menuCloseIcon.setAttribute("class", "menuIconHide")
+    menuIcon.setAttribute("class", "menuIconShow")
   }
-  console.log("???")
 }
