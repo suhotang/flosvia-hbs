e633a948baeada4a4c1f5aed6d8e6e4bb249d3f1
suhotang <dkdudkc22@gmail.com>
2022-05-29 21:05:39 +0900

    Fix   - tailwind npm 모듈 추가 + postcss(크로스 브라우징용?) 모듈 추가   - tailwind, postcss 설정 파일 추가, package.json에 css 컴파일 하는 스크립트 추가   - tailwind cdn script 태그 삭제   - 메인 페이지 콘텐츠 변경     - 상단에 배너? 같은 거 추가     - 배너 밑에 카드 슬라이더 뷰가 있음 (카드 크기를 키움)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
+  content: ["./views/**/*.hbs"],
   theme: {
     colors: {
       primary: "#e73b63",
       secondary: "#eb6282",
-      interaction: rgba(161, 41, 69, 0.3),
+      interaction: "rgba(161, 41, 69, 0.3)",
     },
+    extend: {},
   },
+  plugins: [],
 }
