db2e850be062c485403a6bbbbbf8a682ad7a4cbe
suhotang <dkdudkc22@gmail.com>
2022-06-23 23:47:57 +0900

    Feat(로그인 요청) - 로그인 페이지에서 로그인 요청하는 fetch 로직 작성

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,12 +1,46 @@
+function parseJwt(token) {
+  var base64Url = token.split(".")[1]
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/")
+  var jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2)
+      })
+      .join("")
+  )
+
+  return JSON.parse(jsonPayload)
+}
+
 export function loginHandler(apiUrl) {
   const loginBtnElement = document.getElementById("login-submit-btn")
   loginBtnElement.addEventListener("click", () => {
-    console.log("click btn!")
-
     const email = document.getElementById("email-input").value
     const password = document.getElementById("pwd-input").value
-    console.log(email, password)
 
-    // fetch
+    fetch(`${apiUrl}/api/auth/signin`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then(async (response) => {
+        const data = await response.json()
+        const { accessToken, message } = data || {}
+
+        if (accessToken) {
+          console.log(parseJwt(accessToken))
+          localStorage.setItem("auth", JSON.stringify({ accessToken }))
+        }
+        alert(message)
+      })
+      .catch((error) => {
+        console.log(error)
+        alert("로그인 실패!")
+      })
   })
 }
